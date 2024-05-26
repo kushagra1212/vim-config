b562aae7de63aeefd54c7c5ede09c63db9249cdd
Kushagra Rathore <71135665+kushagra1212@users.noreply.github.com>
2024-05-26 16:04:09 +0530

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+vim.cmd("autocmd BufRead,BufNewFile *.fg set filetype=fg")
 vim.api.nvim_set_keymap("v", "<C-/>", ":norm I//<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("v", "<U-/>", ":s!^//!!<CR>", { noremap = true, silent = true })
 -- Delete without yanking
